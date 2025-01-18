ffab99d9cbaec2b0faebb625431e2feb1469692a
Khalil Hafz <khalilhafz@gmail.com>
2025-01-18 20:39:04 +0200

    update js

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let btn = document.getElementById("up");
+
+
+
+window.onscroll = function () {
+    if (window.scrollY >= 600) { 
+        btn.style.display = "block";
+    }
+    else {
+        btn.style.display = "none";
+
+
+    }
+};
+
+
+
+btn.onclick = function () {
+    window.scrollTo({
+        right: 0,
+        top : 0,
+        behavior: "smooth"
+    });
+};
